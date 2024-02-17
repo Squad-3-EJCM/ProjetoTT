05953ab063c4b69ffc84507ffc3dab7b3c3bc22a
Leonardo <leonardodebarros0@gmail.com>
2024-02-17 18:56:51 -0300

    feature: teste do cadastro

--- frontend/src/pages/Cadastro/index.tsx
+++ frontend/src/pages/Cadastro/index.tsx
@@ -1,28 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import {Image, Text, View, TextInput, Pressable} from 'react-native';
-import {styles} from './style';
+import { Botao2, BotaoTexto2, Container, Container2, Email, Input, Logo, Senha } from './style';
 
 export default function Cadastro() {
-  return (
-    <View style={styles.container}>
-        <Image source={require("../../assets/Logo.png")} style={styles.logo}/>
-        <View style={styles.container2}>
-            <Image source={require("../../assets/email.png")} style={styles.email}/>
-            <TextInput placeholder='E-mail' style={styles.input}/>
-        </View>
-        <View style={styles.container2}>
-            <Image source={require("../../assets/cadeado.png")} style={styles.senha}/>
-            <TextInput placeholder='Senha' style={styles.input}/>
-        </View>
-        <View style={styles.container2}>
-            <Image source={require("../../assets/cadeado.png")} style={styles.senha}/>
-            <TextInput placeholder='Confirmar Senha' style={styles.input}/>
-        </View>
-      
+    return (
+      <Container>
+        <Logo source={require("../../assets/Logo.png")}/>
 
-        
-        <Pressable style={styles.botao2}><Text style={styles.botaoTexto2}>Cadastrar-se</Text></Pressable>
-        
-    </View>
-  );
+        <Container2>
+          <Email source={require("../../assets/email.png")}/>
+          <Input placeholder='E-mail'/>
+        </Container2>
+
+        <Container2>
+          <Senha source={require("../../assets/cadeado.png")}/>
+          <Input placeholder='Senha'/>
+        </Container2>
+
+        <Container2>
+          <Senha source={require("../../assets/cadeado.png")}/>
+          <Input placeholder='Confirmar Senha'/>
+        </Container2>
+  
+        <Botao2><BotaoTexto2>Cadastrar-se</BotaoTexto2></Botao2>
+      </Container>
+    );
 }
