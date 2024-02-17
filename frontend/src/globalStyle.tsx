88751dd26af4210c21b2b66ee3f09803e45c4728
Iago <iagocesarts@hotmail.com>
2024-02-16 21:01:10 -0300

    refactor: Setup style-components.

---
+++ frontend/src/globalStyle.tsx
@@ -0,0 +1,12 @@
+export const global = {
+    colors: {
+        black: '#0E0D0E',
+        azul: '#45497C',
+        purple: '#7122EE',
+        green: '#DEFA63',
+        gray: '#F1F1E6'
+    },
+    fonts: {
+        
+    }
+}
