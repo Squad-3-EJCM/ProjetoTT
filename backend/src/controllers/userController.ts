957c3979470076baeb3b0505647eba9dd06f35b8
Jorge Santiago <jsantiago12tone@gmail.com>
2024-02-22 02:14:23 -0300

    feature: add a controller for the user model

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient({});
+
+import { Request, Response } from 'express';
+
+async function createUser(request:Request, response:Response) {
+  try {
+    const user = await prisma.user.create(
+      {data: request.body}
+    )
+    response.status(201).json(user);
+  }
+  catch(error) {
+    response.status(500).json({error:error})
+  }
+}
+
+async function readUser(request:Request, response:Response) {
+  const {id} = request.params;
+  try {
+    const user = await prisma.user.findUnique(
+      {where: {id: String(id)}}
+    )
+    response.status(200).json(user);
+  }
+  catch(error) {
+    response.status(500).json({error:error})
+  }
+}
+
+async function updateUser(request:Request, response:Response) {
+  const {id} = request.params;
+  try {
+    const user = await prisma.user.update(
+      {
+        data: request.body,
+        where: {id: String(id)}
+      }
+    )
+    response.status(200).json(user);
+  }
+  catch(error) {
+    response.status(500).json({error:error})
+  }
+}
+
+export {
+  createUser,
+  readUser,
+  updateUser
+}
